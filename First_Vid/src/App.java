cd947447c3bd905bccc598c4548c859288ca6614
khagendra01 <khagendrakhatri365@gmail.com>
2024-01-23 22:40:44 -0500

    sec

--- First_Vid/src/App.java
+++ First_Vid/src/App.java
@@ -1,4 +1,40 @@
 public class App {
+    public class OopTut {
+
+        public class Man{
+    
+            private String name;
+            private int age;  
+            private char gender;  
+    
+            public Man()
+            {
+                name = "";
+                age = 0;
+                gender = 'm';
+            }
+    
+            public Man(String name, int age, char gender)
+            {
+                this.name = name;
+                this.age = age;
+                this.gender = gender;
+            }
+    
+    
+        }
+    
+        public void main(String[] args) {
+    
+            Man newman = new Man();
+    
+            System.out.println(newman.name);
+    
+    
+        }
+    
+    }
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
